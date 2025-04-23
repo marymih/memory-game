1cbeb9d0470d54d5d629a2e0cd90f3382f7b8457
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2025-04-23 16:08:49 +0200

    generate random indices, shuffle emojis

--- App.jsx
+++ App.jsx
@@ -20,13 +20,57 @@ export default function App() {
       }
 
       const data = await response.json();
-      const dataSample = data.slice(0, 5);
+      const dataSlice = await getDataSlice(data);
+      const emojisArray = await getEmojisArray(dataSlice);
 
-      setEmojisData(dataSample);
+      setEmojisData(emojisArray);
       setIsGameOn(true);
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async function getDataSlice(data) {
+    const randomIndices = getRandomIndices(data);
+
+    const dataSlice = randomIndices.reduce((array, index) => {
+      array.push(data[index]);
+      return array;
+    }, []);
+
+    return dataSlice;
+  }
+
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNum)) {
+        randomIndicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+    return randomIndicesArray;
+  }
+
+  async function getEmojisArray(data) {
+    const pairedEmojisArray = [...data, ...data];
+
+    return shuffleArray(pairedEmojisArray);
+  }
+
+  function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
   }
 
   function turnCard() {
